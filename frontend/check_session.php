6c5c93063cf66eab22341aee90d4c90b8f571c84
rafirhnnn <rafirhn17@gmail.com>
2025-01-28 20:18:14 +0700

    session

--- frontend/check_session.php
+++ frontend/check_session.php
@@ -1,6 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
-<<<<<<< HEAD
 function checkSession() {
 
     const formData = new FormData();
@@ -24,29 +23,4 @@ function checkSession() {
 }
 
 checkSession();
-=======
-    function checkSession() {
-
-        const formData = new FormData();
-        formData.append('session_token', localStorage.getItem('session_token'));
-
-        axios.post('http://localhost/UtbKosWeb/backend/session.php', formData)
-            .then(response => {
-                console.log(response);
-                if (response.data.status === 'success') {
-                    const nama = response.data.hasil.name || 'Default Name';
-                    localStorage.setItem('nama', nama);
-                } else {
-
-                    window.location.href = 'login.php';
-                }
-            })
-            .catch(error => {
-
-                console.error('Error checking session:', error);
-            });
-    }
-
-    checkSession();
->>>>>>> b9e88bda990e8a45fdcd952423decd2a0cc4da0c
 </script>
